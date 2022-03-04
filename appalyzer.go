606b1be3cf147fa43d439e046de20d2659860ae8
Joel Pingul <kirckpingul@gmail.com>
2022-03-04 14:50:33 -0800

    added cli, added source, and git ignore file

--- appalyzer.go
+++ appalyzer.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	fmt.Println("hello world!")
+		//Initial CLI App Setup
+	app := &cli.App{
+		Name:        "Appalyzer",
+		Version:     "0.1.0",
+		Description: "Static analysis & Threat modeling tool",
+		Authors: []*cli.Author{
+			{Name: "KP",},
+		},
+		Flags: []cli.Flag{
+			&cli.StringFlag{Name: "web", Value: "no", Usage: "Enable web server for GUI", Required: false,},
+			&cli.StringFlag{Name: "filepath", Value: "", Usage: "Choose a app project to analyze", Required: false,},
+		},
+		Action: func(c *cli.Context) error {
+
+		     	return nil
+	    	},
+	}
+
+
+
+	//Run CLI
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
